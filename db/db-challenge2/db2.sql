10e05b80f896d8622e9a6581694ca9bb47676d66
39nina <62025642+39nina@users.noreply.github.com>
2020-06-30 13:06:29 +0900

    Fix：データベース定義書、DDL文のテーブル名、カラム名を同内容で修正

--- db/db-challenge2/db2.sql
+++ db/db-challenge2/db2.sql
@@ -1,5 +1,5 @@
 BEGIN;
-CREATE TABLE user_data (
+CREATE TABLE users_data (
 user_id INTEGER PRIMARY KEY AUTO_INCREMENT,
 mail VARCHAR(100) NOT NULL,
 pass VARCHAR(100) NOT NULL,
@@ -7,50 +7,50 @@ user_name VARCHAR(100) NOT NULL,
 introduce VARCHAR(1000),
 tel_work VARCHAR(13),
 tel_mobile VARCHAR(13),
-delete_flg TINYINT NOT NULL DEFAULT 0,
+is_deleted TINYINT NOT NULL DEFAULT 0,
 created_at DATETIME NOT NULL,
-update_at DATETIME NOT NULL
+updated_at DATETIME NOT NULL
 );
-CREATE TABLE chat_join (
+CREATE TABLE chatrooms_join (
 chatroom_id INTEGER NOT NULL,
-part_id INTEGER NOT NULL,
-part_time DATETIME NOT NULL,
+join_user_id INTEGER NOT NULL,
+joined_on DATETIME NOT NULL,
 PRIMARY KEY(chatroom_id, part_id)
 );
-CREATE TABLE chatroom (
+CREATE TABLE chatrooms (
 chatroom_id INTEGER PRIMARY KEY AUTO_INCREMENT,
 chatroom_name VARCHAR(100) NOT NULL,
 summary VARCHAR(1000) NOT NULL,
-file_permit_flg TINYINT NOT NULL DEFAULT 1,
-dc_flg TINYINT NOT NULL DEFAULT 0,
-delete_flg TINYINT NOT NULL DEFAULT 0,
+is_permitted_sending_file TINYINT NOT NULL DEFAULT 1,
+is_directchat TINYINT NOT NULL DEFAULT 0,
+is_deleted TINYINT NOT NULL DEFAULT 0,
 creator_id INTEGER NOT NULL REFERENCES user_data(user_id),
 changer_id INTEGER NOT NULL REFERENCES user_data(user_id),
 created_at DATETIME NOT NULL,
-update_at DATETIME NOT NULL
+updated_at DATETIME NOT NULL
 );
-CREATE TABLE post (
+CREATE TABLE posts (
 post_id INTEGER PRIMARY KEY AUTO_INCREMENT,
 post_detail VARCHAR(1000) NOT NULL,
 file_name VARCHAR(100),
 chatroom_id INTEGER NOT NULL REFERENCES chatroom(chatroom_id),
-delete_flg TINYINT DEFAULT 0 NOT NULL,
+is_deleted TINYINT DEFAULT 0 NOT NULL,
 poster_id INTEGER NOT NULL REFERENCES user_data(user_id),
 changer_id INTEGER NOT NULL REFERENCES user_data(user_id),
 created_at DATETIME NOT NULL,
-update_at DATETIME NOT NULL
+updated_at DATETIME NOT NULL
 );
-CREATE TABLE task (
+CREATE TABLE tasks (
 task_id INTEGER PRIMARY KEY AUTO_INCREMENT,
 task_detail VARCHAR(1000) NOT NULL,
 task_maneger_id INTEGER NOT NULL REFERENCES user_data(user_id),
 chatroom_id INTEGER NOT NULL REFERENCES chatroom(chatroom_id),
-deadline DATETIME,
-close_flg TINYINT DEFAULT 0 NOT NULL,
-delete_flg TINYINT DEFAULT 0 NOT NULL,
+closed_on DATETIME,
+is_closed TINYINT DEFAULT 0 NOT NULL,
+is_deleted TINYINT DEFAULT 0 NOT NULL,
 creator_id DATETIME NOT NULL REFERENCES user_data(user_id),
 changer_id DATETIME NOT NULL REFERENCES user_data(user_id),
 created_at DATETIME NOT NULL,
-update_at DATETIME NOT NULL
+updated_at DATETIME NOT NULL
 );
 COMMIT;
